e6352cc2bad4e26e9e491e744946834563a6a65e
arjae.sj <61751130+arjaesj@users.noreply.github.com>
2020-07-10 01:24:04 +0800

    wrote ReadMe format

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,41 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ![badge](https://img.shields.io/badge/License-${data.license}-green.svg)
+
+## Description
+${data.description}
+
+## Table of Contents
+1. [Installation](#Installation)
+2. [Usage](#Usage)
+3. [License](#License)
+3. [Contributing](#Contributing)
+3. [Test](#Test)
+3. [Questions](#Questions)
+
+
+### Installation
+${data.installation}
+
+### Usage
+Deployed URL: ${data.deployedURL}  
+${data.usage}
+
+### License
+Copyright (c) 2012 ${data.fullname} (GitHub profile name: ${data.githubName})  
+Licensed under the ${data.license} license.
+
+### Contributing
+${data.contributing}
+
+### Test
+![demo](${data.test})
+
+### Questions
+Any questions regarding this project, please don't hesitate to contact me:  
+Email me @ ${data.email}  
+My GitHub profile page: https://github.com/${data.githubName}  
 
 `;
 }
